779014e2a25fcf0baea577218818602f588aa62d
Wyat Soule <wsoule@wyats-mbp.lan>
2021-08-09 10:45:47 -0400

    add boxes and bullet points

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// alert('hello');
+/*
+let myImage = document.querySelector('controller');
+myImage.onclick = function(){
+    let srcChange = myImage.getAttribute('src');
+    if(srcChange === 'file:///Users/wsoule/Sites/dudeman/assets/controller.jpeg'){
+        myImage.setAttribute('src', 'file:///Users/wsoule/Sites/dudeman/assets/smile.png');
+    } else {
+        myImage.setAttribute('src', 'file:///Users/wsoule/Sites/dudeman/assets/controller.jpeg')
+    }
+}
+
+let myButton = document.querySelector('First Button');
+let myHeading = document.getElementById('change');
+myButton.onclick = function(){
+    myHeading.textContent = 'you changed';
+}
+*/
