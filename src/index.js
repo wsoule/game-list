e5f419556a19e1eada45b5b945ce3770ce404406
Wyat Soule <wsoule@wyats-mbp.lan>
2021-08-18 16:07:16 -0400

    add back end and front end communication

--- src/index.js
+++ src/index.js
@@ -7,10 +7,13 @@ function addNewGameUnplayed(){
     let gameTitle = prompt('Game Title');
     addGame(unplayedList,gameTitle);
 }
-function addNewGamePlayed(){
+async function addNewGamePlayed(){
     let gameTitle = prompt('Game Title');
-    addGame(playedList,gameTitle);
-    removeGame(unplayedList, gameTitle);
+    const getText = await postText(`${environment.urls.api}/add-played-game`, gameTitle);
+    //getJson(`${environment.urls.api}/add-played-game`)
+    //addGame(playedList,await getText.text());
+    console.log((await getText.text()));
+    // removeGame(unplayedList, gameTitle);
 }
 function makeListofGames(list, games){
     games.forEach((game) => addGame(list,game)); 
@@ -22,6 +25,15 @@ function addGame(list, gameName){
     const listItem = document.createElement('li');
     listItem.innerText = gameName;
     list.appendChild(listItem);
+}
+function postText(url, body){
+    return fetch(url, {
+        body: body,
+        headers: {
+            'Content-Type':'text/plain'
+        },
+        method: 'POST'
+    });
 }
 function removeGame(list, gameName){
     Array.from(list.children).forEach((listItem) => {
@@ -35,6 +47,9 @@ const unplayedList = document.getElementById('unplayed');
 
 async function getJson(url){
     return (await fetch(url)).json();
+}
+async function getString(url){
+    return (await fetch(url)).toString();
 }
 async function main(){
     const controller = document.getElementById('controller');
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const fs = require('fs');
 const app = express();
@@ -7,19 +8,30 @@ app.use((request, response, next) => {
     response.header('Access-Control-Allow-Origin', '*');
     response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
-  });
+});
+app.use(express.json());
+app.use(express.text());
+app.use('/assets', express.static('assets'));
+
 app.get('/played-games', (req, res) => {
     fs.readFile(__dirname + '/../data/played-games.json', 'utf8' , (err, data) => {
         res.send(data);
     });
 });
+
 app.get('/unplayed-games', (req, res) => {
     fs.readFile(__dirname + '/../data/unplayed-games.json', 'utf8' , (err, data) => {
         res.send(data);
     });
 });
+
+app.post('/add-played-game', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
+
 app.get('/', (req,res) => res.send("hello again"));
-app.use('/assets', express.static('assets'));
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
